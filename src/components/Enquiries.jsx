64328fa447951c8c0fdf575863c4cfab756fdbc5
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-04-17 19:17:59 +0530

    done

--- src/components/Enquiries.jsx
+++ src/components/Enquiries.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { 
-  CheckCircle2, Clock, ChevronRight, RefreshCw, Square, CheckSquare, X, Loader2 ,Search ,Eye ,Trash2 ,Pencil ,Plus ,Phone
+  CheckCircle2, Clock, ChevronRight, RefreshCw, Square, CheckSquare, X, Loader2 ,Search ,Eye ,Trash2 ,Pencil ,Plus ,Phone ,Mail
 } from 'lucide-react';
 
 const Enquiries = () => {
